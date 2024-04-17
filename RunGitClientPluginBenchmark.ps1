3e9d29c47a6bc5baa685d618cb45713a8a16c7bd
Chris Herczeg <cherczeg@purdue.edu>
2024-04-16 19:37:37 -0500

    nit

--- RunGitClientPluginBenchmark.ps1
+++ RunGitClientPluginBenchmark.ps1
@@ -7,40 +7,52 @@
 
 function InstallOpenJdk21()
 {
-    mkdir OpenJdk21 -ErrorAction SilentlyContinue
+    if (-not(Test-Path $PSScriptRoot/OpenJdk21))
+    {
+        mkdir OpenJdk21 -ErrorAction SilentlyContinue
     
-    Invoke-WebRequest -OutFile $PSScriptRoot/OpenJdk21/OpenJdk21.zip -Uri https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip
+        Invoke-WebRequest -OutFile $PSScriptRoot/OpenJdk21/OpenJdk21.zip -Uri https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_windows-x64_bin.zip
 
-    Expand-Archive $PSScriptRoot/OpenJdk21/OpenJdk21.zip -DestinationPath $PSScriptRoot/OpenJdk21
+        Expand-Archive $PSScriptRoot/OpenJdk21/OpenJdk21.zip -DestinationPath $PSScriptRoot/OpenJdk21
+    }
 }
 
 function InstallMicrosoftJdk21()
 {
-    mkdir MicrosoftJdk21 -ErrorAction SilentlyContinue
+    if (-not(Test-Path $PSScriptRoot/MicrosoftJdk21))
+    {
+        mkdir MicrosoftJdk21 -ErrorAction SilentlyContinue
 
-    Invoke-WebRequest -OutFile $PSScriptRoot/MicrosoftJdk21/MicrosoftJdk21.zip -Uri https://aka.ms/download-jdk/microsoft-jdk-21.0.2-windows-x64.zip
+        Invoke-WebRequest -OutFile $PSScriptRoot/MicrosoftJdk21/MicrosoftJdk21.zip -Uri https://aka.ms/download-jdk/microsoft-jdk-21.0.2-windows-x64.zip
 
-    Expand-Archive $PSScriptRoot/MicrosoftJdk21/MicrosoftJdk21.zip -DestinationPath $PSScriptRoot/MicrosoftJdk21
+        Expand-Archive $PSScriptRoot/MicrosoftJdk21/MicrosoftJdk21.zip -DestinationPath $PSScriptRoot/MicrosoftJdk21
+    }
 }
 
 function InstallMaven()
 {
-    mkdir Maven -ErrorAction SilentlyContinue
+    if (-not(Test-Path $PSScriptRoot/Maven))
+    {
+        mkdir Maven -ErrorAction SilentlyContinue
 
-    Invoke-WebRequest -OutFile $PSScriptRoot/Maven/Maven.zip -Uri https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip
+        Invoke-WebRequest -OutFile $PSScriptRoot/Maven/Maven.zip -Uri https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip
 
-    Expand-Archive $PSScriptRoot/Maven/Maven.zip -DestinationPath $PSScriptRoot/Maven
+        Expand-Archive $PSScriptRoot/Maven/Maven.zip -DestinationPath $PSScriptRoot/Maven
+    }
 
     $env:PATH="$PSScriptRoot\Maven\apache-maven-3.9.6\bin;$env:PATH"
 }
 
 function InstallNode()
 {
-    mkdir node -ErrorAction SilentlyContinue
+    if (-not(Test-Path $PSScriptRoot/node))
+    {
+        mkdir node -ErrorAction SilentlyContinue
     
-    Invoke-WebRequest -OutFile $PSScriptRoot/Node/Node.zip -Uri https://nodejs.org/dist/v20.12.2/node-v20.12.2-win-x64.zip
+        Invoke-WebRequest -OutFile $PSScriptRoot/Node/Node.zip -Uri https://nodejs.org/dist/v20.12.2/node-v20.12.2-win-x64.zip
 
-    Expand-Archive $PSScriptRoot/Node/Node.zip -DestinationPath $PSScriptRoot/Node
+        Expand-Archive $PSScriptRoot/Node/Node.zip -DestinationPath $PSScriptRoot/Node
+    }
 
     $env:PATH="$PSScriptRoot\node_modules\npm\bin;$env:PATH"
 
@@ -49,12 +61,18 @@ function InstallNode()
 
 function CloneGitPluginRepo()
 {
-    git clone https://github.com/jenkinsci/git-client-plugin.git GCP
+    if (-not(Test-Path $PSScriptRoot/GCP))
+    {
+        git clone https://github.com/jenkinsci/git-client-plugin.git GCP
+    }
 }
 
 function CloneGitPluginPerformanceFork()
 {
-    git clone https://github.com/chrisherczeg/oss-performance-git-client-plugin.git GCP_Perf
+    if (-not(Test-Path $PSScriptRoot/GCP_Perf))
+    {
+        git clone https://github.com/chrisherczeg/oss-performance-git-client-plugin.git GCP_Perf
+    }
 }
 
 function RunOpenJdkBenmarkMaster()
